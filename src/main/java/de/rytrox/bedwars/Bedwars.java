a1f7647fd0ee5b9cf5dcb02f586467059c83f4df
Timeout <taubmann870@gmail.com>
2021-06-16 10:25:21 +0200

    Add Main-Class

---
+++ src/main/java/de/rytrox/bedwars/Bedwars.java
@@ -0,0 +1,56 @@
+package de.rytrox.bedwars;
+
+import de.timeout.libs.config.ConfigCreator;
+import de.timeout.libs.config.UTFConfig;
+import de.timeout.libs.log.ColoredLogger;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+public final class Bedwars extends JavaPlugin {
+
+    private UTFConfig config;
+
+    @Override
+    public void onEnable() {
+        // Nutze im Logger ColorCodes mit '&'
+        ColoredLogger.enableColoredLogging('&', getLogger(), "&8[&6Bedwars&8]");
+        // reload config
+        reloadConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        this.saveConfig();
+    }
+
+    @Override
+    public @NotNull UTFConfig getConfig() {
+        return config;
+    }
+
+    @Override
+    public void reloadConfig() {
+        try {
+            File file = new ConfigCreator(this.getDataFolder(), Paths.get(""))
+                .copyDefaultFile(Paths.get("config.yml"), Paths.get("config.yml"));
+
+            this.config = new UTFConfig(file);
+        } catch (IOException e) {
+            this.getLogger().log(Level.SEVERE, "&cconfig.yml konnte nicht geladen werden!", e);
+        }
+    }
+
+    @Override
+    public void saveConfig() {
+        try {
+            this.config.save(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            this.getLogger().log(Level.SEVERE, "&cconfig.yml konnte nicht gespeichert werden");
+        }
+    }
+}
