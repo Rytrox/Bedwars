d619b221d36270fb9dd29a70a108e5a9c3078277
kaigoe <kai.goe@gmx.de>
2021-06-22 19:10:31 +0200

    BW-6: LOL Item Drops Prefektioniert und Armor Stands eingebaut
    LOOOOOOOOOOOOOL

--- src/main/java/de/rytrox/bedwars/utils/RecourceSpawner.java
+++ src/main/java/de/rytrox/bedwars/utils/RecourceSpawner.java
@@ -4,9 +4,13 @@ import de.rytrox.bedwars.Bedwars;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 
 public class RecourceSpawner {
 
@@ -15,6 +19,8 @@ public class RecourceSpawner {
     private Location pos;
     private Material material;
     private BukkitTask task;
+    private ArmorStand armorStand;
+    private Location pRecourceTeleport;
 
     public RecourceSpawner(Material material, Location pos, int time1, int time2, int time3) {
         times[0] = time1;
@@ -23,13 +29,24 @@ public class RecourceSpawner {
         this.pos = pos;
         this.material = material;
         level = 1;
+        Location pArmorStand = pos;
+        pArmorStand.add(0,-1,0);
+        armorStand = pos.getWorld().spawn(pArmorStand, ArmorStand.class);
+        armorStand.setCustomNameVisible(true);
+        armorStand.setCustomName("Level: " + level);
+        armorStand.setVisible(false);
         start();
+        pRecourceTeleport = pos;
+        pRecourceTeleport.add(0,1,0);
+
     }
 
     public void start() {
         task = Bukkit.getServer().getScheduler().runTaskTimer(JavaPlugin.getPlugin(Bedwars.class), () -> {
-            pos.getWorld().dropItemNaturally(pos, new ItemStack(material));
-        }, 20,20*(times[level - 1]));
+            Item dropItem = pos.getWorld().dropItemNaturally(pos, new ItemStack(material));
+            dropItem.setVelocity(new Vector());
+            dropItem.teleport(pRecourceTeleport);
+        }, 100,20*(times[level - 1]));
 
     }
 
@@ -47,6 +64,7 @@ public class RecourceSpawner {
 
     public void levelUp() {
         if(level < 3) {
+            armorStand.setCustomName("Level: " + level);
             this.level++;
             this.stop();
             this.start();
