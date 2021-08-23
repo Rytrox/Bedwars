b7378b7f06c24f9edefe6041b700b40bdf3f4afc
Simon <asedem.games@gmail.com>
2021-08-23 21:03:59 +0200

    BW-8: Convertion Methodes Deleted

--- src/main/java/de/rytrox/bedwars/utils/MapUtils.java
+++ src/main/java/de/rytrox/bedwars/utils/MapUtils.java
@@ -1,13 +1,8 @@
 package de.rytrox.bedwars.utils;
 
 import de.timeout.libs.sql.SQL;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.sql.SQLException;
 
 public class MapUtils {
 
@@ -18,39 +13,12 @@ public class MapUtils {
     }
 
     public void updateTables() {
+        try {
+            db.prepare("CREATE TABLE IF NOT EXISTS Spawner (id VARCHAR NOT NULL, " +
+                    "gold VARCHAR, iron VARCHAR, bronze VARCHAR, PRIMARY KEY (id))").execute();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
-    public String convertLocationToString(Location location) {
-        return String.format("%s ! %f ! %f ! %f ! %f ! %f",
-                Objects.requireNonNull(location.getWorld()).getName(),
-                location.getX(),
-                location.getY(),
-                location.getZ(),
-                location.getYaw(),
-                location.getPitch());
-    }
-
-    public Location convertStringToLocation(String string) {
-        String[] values = string.split(" ! ");
-        if(values.length != 6) return null;
-        return new Location(Bukkit.getWorld(values[0]),
-                Double.parseDouble(values[1]),
-                Double.parseDouble(values[2]),
-                Double.parseDouble(values[3]),
-                Float.parseFloat(values[4]),
-                Float.parseFloat(values[5]));
-    }
-
-    public String convertListToString(List<String> list) {
-        return list.stream()
-                .collect(Collectors.joining("_", "{", "}"));
-    }
-
-    public List<String> convertStringToList(String string) {
-        return Arrays.asList(string.replace("{", "")
-                .replace("}", "")
-                .split("_"));
-    }
-
 }
