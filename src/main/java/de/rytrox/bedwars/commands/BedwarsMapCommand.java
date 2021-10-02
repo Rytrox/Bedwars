10dae0a75199789ec431a2a692818bbe203e4781
Simon <asedem.games@gmail.com>
2021-10-02 13:26:33 +0200

    BW-8: Better command logic

--- src/main/java/de/rytrox/bedwars/commands/BedwarsMapCommand.java
+++ src/main/java/de/rytrox/bedwars/commands/BedwarsMapCommand.java
@@ -1,17 +1,22 @@
 package de.rytrox.bedwars.commands;
 
 import de.rytrox.bedwars.Bedwars;
+import de.rytrox.bedwars.map.Map;
 import de.rytrox.bedwars.map.MapLoadEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class BedwarsMapCommand implements CommandExecutor {
+import java.util.List;
+
+public class BedwarsMapCommand implements CommandExecutor, TabCompleter {
 
     /*
     - (1) bwmap help
@@ -66,6 +71,7 @@ public class BedwarsMapCommand implements CommandExecutor {
 
                         return true;
                     case "save":
+                        if(!isInEditMode(args[1], player)) return true;
 
                         break;
                     case "remove":
@@ -77,9 +83,11 @@ public class BedwarsMapCommand implements CommandExecutor {
                 if ("modify".equals(args[0])) {
                     switch (args[2]) {
                         case "pos1":
+                            if(!isInEditMode(args[1], player)) return true;
 
                             return true;
                         case "pos2":
+                            if(!isInEditMode(args[1], player)) return true;
 
                             return true;
                     }
@@ -89,10 +97,12 @@ public class BedwarsMapCommand implements CommandExecutor {
                 if ("modify".equals(args[0])) {
                     switch (args[2]) {
                         case "teamsize":
+                            if(!isInEditMode(args[1], player)) return true;
 
                             return true;
                         case "spawner":
                             if("remove".equals(args[3])) {
+                                if(!isInEditMode(args[1], player)) return true;
 
                                 return true;
                             }
@@ -105,6 +115,7 @@ public class BedwarsMapCommand implements CommandExecutor {
                     switch (args[2]) {
                         case "spawner":
                             if("add".equals(args[3])) {
+                                if(!isInEditMode(args[1], player)) return true;
 
                                 return true;
                             }
@@ -112,9 +123,11 @@ public class BedwarsMapCommand implements CommandExecutor {
                         case "teams":
                             switch (args[3]) {
                                 case "add":
+                                    if(!isInEditMode(args[1], player)) return true;
 
                                     return true;
                                 case "remove":
+                                    if(!isInEditMode(args[1], player)) return true;
 
                                     return true;
                             }
@@ -126,12 +139,15 @@ public class BedwarsMapCommand implements CommandExecutor {
                 if ("modify".equals(args[0]) && "teams".equals(args[2]) && "modify".equals(args[3])) {
                     switch (args[5]) {
                         case "villager":
+                            if(!isInEditMode(args[1], player)) return true;
 
                             return true;
                         case "bed":
+                            if(!isInEditMode(args[1], player)) return true;
 
                             return true;
                         case "spawn":
+                            if(!isInEditMode(args[1], player)) return true;
 
                             return true;
                     }
@@ -140,6 +156,7 @@ public class BedwarsMapCommand implements CommandExecutor {
                 return true;
             case 7:
                 if ("modify".equals(args[0]) && "teams".equals(args[2]) && "modify".equals(args[3]) && "color".equals(args[5])) {
+                    if(!isInEditMode(args[1], player)) return true;
 
                 }
                 break;
@@ -148,8 +165,20 @@ public class BedwarsMapCommand implements CommandExecutor {
         return true;
     }
 
-    @EventHandler
-    public void onMapLoad(MapLoadEvent event) {
-        main.getMapUtils().getMapsInEdit().add(event.getMap());
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        
+        return null;
+    }
+
+    private boolean isInEditMode(String name, Player player) {
+        boolean statement = true;
+        if(statement) {
+            return true;
+        } else {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cDie Map &7" + name + "&c ist nicht im Editor!"));
+            return false;
+        }
     }
 }
