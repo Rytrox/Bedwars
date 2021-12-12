cc1d22bb016b59bcfef1f99bb3df17193447bb8a
Simon <asedem.games@gmail.com>
2021-12-12 17:01:04 +0100

    BW-28: Events created and registered

--- src/main/java/de/rytrox/bedwars/phase/phases/LobbyPhase.java
+++ src/main/java/de/rytrox/bedwars/phase/phases/LobbyPhase.java
@@ -2,7 +2,7 @@ package de.rytrox.bedwars.phase.phases;
 
 import de.rytrox.bedwars.Bedwars;
 
-import de.rytrox.bedwars.database.repository.TopTenSignsRepository;
+import de.rytrox.bedwars.listeners.LobbyBreakPlaceListener;
 import de.rytrox.bedwars.utils.Countdown;
 import de.rytrox.bedwars.utils.TopTenBoardHandler;
 import org.bukkit.Bukkit;
@@ -13,12 +13,14 @@ public class LobbyPhase extends GamePhase {
 
     private final Countdown countdown;
     private final TopTenBoardHandler topTenBoardHandler;
+    private final LobbyBreakPlaceListener lobbyBreakPlaceListener;
 
     public LobbyPhase(Bedwars main) {
         super(main);
 
         this.countdown = new Countdown(main);
         this.topTenBoardHandler = new TopTenBoardHandler();
+        this.lobbyBreakPlaceListener = new LobbyBreakPlaceListener();
     }
 
     /**
@@ -29,6 +31,7 @@ public class LobbyPhase extends GamePhase {
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this.countdown, main);
         Bukkit.getPluginManager().registerEvents(this.topTenBoardHandler, main);
+        Bukkit.getPluginManager().registerEvents(this.lobbyBreakPlaceListener, main);
     }
 
     /**
@@ -39,6 +42,7 @@ public class LobbyPhase extends GamePhase {
     public void onDisable() {
         HandlerList.unregisterAll(countdown);
         HandlerList.unregisterAll(topTenBoardHandler);
+        HandlerList.unregisterAll(lobbyBreakPlaceListener);
     }
 
     /**
