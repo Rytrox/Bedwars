02b490705e16299f7195453fd7933ad902ef5812
Simon <asedem.games@gmail.com>
2021-12-14 18:36:05 +0100

    BW-28: Added other events

--- src/main/java/de/rytrox/bedwars/listeners/LobbyBreakPlaceListener.java
+++ src/main/java/de/rytrox/bedwars/listeners/LobbyBreakPlaceListener.java
@@ -3,9 +3,22 @@ package de.rytrox.bedwars.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerBucketEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.world.StructureGrowEvent;
 
 public class LobbyBreakPlaceListener implements Listener {
+
+    @EventHandler
+    public void onBlockGrow(BlockGrowEvent event) {
+        event.setCancelled(true);
+    }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
@@ -16,4 +29,35 @@ public class LobbyBreakPlaceListener implements Listener {
     public void onBlockPlace(BlockPlaceEvent event) {
         event.setCancelled(true);
     }
+
+    @EventHandler
+    public void onBlockSpread(BlockSpreadEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onStructureGrow(StructureGrowEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        event.setCancelled(true);
+    }
+
 }
