9ecf8df3a885143966275e0e84a634a0b692687b
kaigoe <kai.goe@gmx.de>
2021-12-15 01:04:57 +0100

    BW-32: Eigentlich Task aber Task will nicht :(

---
+++ src/main/java/de/rytrox/bedwars/map/MapLoader.java
@@ -0,0 +1,29 @@
+package de.rytrox.bedwars.map;
+
+import de.rytrox.bedwars.Bedwars;
+import de.rytrox.bedwars.database.entity.Map;
+import org.bukkit.Bukkit;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class MapLoader {
+
+    private final Bedwars main;
+    private Map map;
+
+    public MapLoader(Bedwars main){
+        this.main = main;
+        Bukkit.getScheduler().runTaskAsynchronously(main,()->{
+            main.getMapRepository().findAllMaps().stream()
+                    .collect(Collectors.collectingAndThen(Collectors.toList(), collected -> {
+                        Collections.shuffle(collected);
+                        return collected.stream();
+                    }))
+                    .findAny()
+                    .ifPresentOrElse(mapList -> map = mapList, () -> System.out.println("No Map Found"));
+        });
+    }
+
+    public Map getMap(){return map;};
+}
