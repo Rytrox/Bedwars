66ae43ed77049530cd24d6c18da3970f7cfbf106
Simon <asedem.games@gmail.com>
2021-12-31 15:07:12 +0100

    BW-29: Player Respawns at Team spawn point & Final kill dosen't respawns

--- src/main/java/de/rytrox/bedwars/listeners/KillDeathListener.java
+++ src/main/java/de/rytrox/bedwars/listeners/KillDeathListener.java
@@ -4,6 +4,8 @@ import de.rytrox.bedwars.Bedwars;
 import de.rytrox.bedwars.database.entity.Team;
 import de.rytrox.bedwars.team.TeamManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -39,7 +41,22 @@ public class KillDeathListener implements Listener {
         }
 
         Team team = teamManager.getTeamByPlayer(player);
-        if (team != null) event.setDeathMessage(team.getColor() + event.getDeathMessage());
+        if (team != null) {
+            player.spigot().respawn();
+            player.teleport(team.getSpawn().toBukkitLocation());
+            player.setGameMode(GameMode.SPECTATOR);
+            if (team.hasBed()) {
+                event.setDeathMessage(team.getColor() + event.getDeathMessage());
+                player.sendTitle(ChatColor.RED + "Du bist tot!", ChatColor.YELLOW + "Du respawnst in 5 Sekunden...", 20, 20, 20);
+                Bukkit.getServer().getScheduler().runTaskLater(main, () -> {
+                    player.teleport(team.getSpawn().toBukkitLocation());
+                    player.setGameMode(GameMode.SURVIVAL);
+                }, 20 * 5);
+            } else {
+                event.setDeathMessage(team.getColor() + event.getDeathMessage() + ChatColor.BOLD + " (FINAL KILL)");
+                player.sendTitle(ChatColor.DARK_RED + "Du bist tot!", ChatColor.GOLD + "Du bist nun ein Zuschauer", 20, 20, 20);
+            }
+        }
     }
 
     @EventHandler
