e7a503e6c58abc4b7f9c8ee10bb2f5e0bf4607be
Kai <kai.goe@gmx.de>
2022-01-08 01:21:35 +0100

    BW-27: i have verbettert it :)

--- src/main/java/de/rytrox/bedwars/utils/LobbyArea.java
+++ src/main/java/de/rytrox/bedwars/utils/LobbyArea.java
@@ -3,6 +3,7 @@ package de.rytrox.bedwars.utils;
 import de.rytrox.bedwars.Bedwars;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -12,18 +13,25 @@ public class LobbyArea {
     private Location start;
     private Location end;
     private BukkitTask moveTask;
+    private Bedwars main;
 
     public LobbyArea(Bedwars main) {
-        start = new Location(Bukkit.getWorld(main.getConfig().getString("lobby.world","world")),main.getConfig().getInt("lobby.pos1.x", 10), main.getConfig().getInt("lobby.pos1.y", 10) ,main.getConfig().getInt("lobby.pos1.z", 10));
-        end = new Location(Bukkit.getWorld(main.getConfig().getString("lobby.world","world")),main.getConfig().getInt("lobby.pos2.x", -10), main.getConfig().getInt("lobby.pos2.y", 100) ,main.getConfig().getInt("lobby.pos2.z", -10));
-        spawn = new Location(Bukkit.getWorld(main.getConfig().getString("lobby.world","world")),main.getConfig().getInt("lobby.spawn.x", 0), main.getConfig().getInt("lobby.spawn.y", 65) ,main.getConfig().getInt("lobby.spawn.z", 0));
+        this.main = main;
         moveTask = Bukkit.getScheduler().runTaskTimer(main, () -> {
             Bukkit.getOnlinePlayers().forEach(player -> {
+                teleport(player.getWorld());
                 if (!Area.inArea(start, end, player.getLocation())) {
                     player.teleport(spawn);
                 }
             });
         }, 0, 30);
+    }
+
+    private void teleport(World world){
+        start = new Location(world, main.getConfig().getInt("lobby.pos1.x", 10), main.getConfig().getInt("lobby.pos1.y", 10) ,main.getConfig().getInt("lobby.pos1.z", 10));
+        end = new Location(world, main.getConfig().getInt("lobby.pos2.x", -10), main.getConfig().getInt("lobby.pos2.y", 100) ,main.getConfig().getInt("lobby.pos2.z", -10));
+        spawn = new Location(world, main.getConfig().getInt("lobby.spawn.x", 0), main.getConfig().getInt("lobby.spawn.y", 65) ,main.getConfig().getInt("lobby.spawn.z", 0));
+
     }
 
     public void stopMoveTask() {
